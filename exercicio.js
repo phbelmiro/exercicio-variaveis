b562877e93558bb400e92570f13a2cd730b9f3f6
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-08-23 21:53:58 -0300

    Update exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -2,16 +2,39 @@
 *pedro belmiro
 */
 
-//1. resposta: 10 "5"
-
-//2. resposta: 20 "10" "20"
-
-//3. resposta: horaPorTabralho
-
-
-
-
 //1
-//A.
-//b.
-//C.
+
+//let nome = prompt("Qual e seu nome?")
+//let idade = prompt("Quantos anos voce tem?")
+
+//console.log(`ola` ,nome, `voce tem` ,idade, `anos`)
+
+//2
+
+//let agua = prompt("voce ja bebeu água hoje?")
+//let livro = prompt("voce ja leu hoje?")
+//let roupa =prompt("voce esta de roupa preta?")
+
+//console.log("voce ja bebeu água hoje? -",agua,)
+//console.log( "voce ja leu hoje? -",livro)
+//console.log("voce esta de roupa preta? -",roupa)
+
+//3
+
+//let A = 10
+//let B = 25
+//let c = null
+
+
+// c = A
+// A = B
+// B = c
+
+//console.log(B, A)
+
+//4
+
+let  = prompt("")
+let  = prompt("Q")
+
+alert(`${periodo/horas} `)
